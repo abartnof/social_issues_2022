864436f8932c4a44094efa76b6c786d3823a2fa7
abartnof <andrewbartnof@gmail.com>
2022-05-31 10:43:59 -0700

    subheader didnt display corrently in readme

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -1,6 +1,13 @@
-#library(MASS)
-#library(lavaan)
+# Analysis of two general questions going into the 2022 midterm elections:
+#  1. How well do people's abstract convictions 
+#     (eg are you conservative or liberal?) truly hang together? Do the two major
+#     parties truly do a decent job of explaining these convictions?
+#  2. What are people's thoughts on the social issues that look to define the 2022
+#     midterm elections, notably abortion?
+
 library(conflicted)
+library(GGally)
+library(ggrepel)
 library(likert)
 library(psych)
 library(skimr)
@@ -12,10 +19,9 @@ conflicted::conflict_prefer('filter', 'dplyr')
 load(file = 'data/clean_data.RData')
 
 # Overall stats 
-StatewisePartyProp <-
+StatewisePartyReg <-
 	Data %>%
 		select(state, party = 'PARTY OF REGISTRATION', weight) %>%
-		filter(party %in% c('Democratic party', 'Republican party')) %>%
 		drop_na %>%
 		group_by(state, party) %>%
 		summarize(subtotal = sum(weight)) %>%
@@ -23,36 +29,52 @@ StatewisePartyProp <-
 		group_by(state) %>%
 		mutate(prop = subtotal / sum(subtotal)) %>%
 		ungroup %>%
+		filter(party %in% c('Democratic party', 'Republican party')) %>%
+		select(state, party, prop) %>%
+		spread(party, prop) %>%
 		mutate(
-			binned_prop = cut_width(prop, width = 0.25, boundary = 0, ordered_result = TRUE)) %>%
-		filter(party == 'Democratic party') %>%
-		select(state, prop_democratic_party = prop, binned_prop)
+			adv_dem = `Democratic party` - `Republican party`,
+		)
 
-USAMap %>%
-	left_join(StatewisePartyProp, by = 'state') %>%
-	ggplot(aes(x = x, y = y, fill = binned_prop, group = group)) +
-	geom_polygon() +
-	scale_fill_manual(na.value = 'grey', values = c('darkred', 'pink', 'lightblue', 'darkblue'),
-										breaks = levels(StatewisePartyProp$binned_prop),
-										labels = c('0% - 25%', '25% - 50%', '50% - 75%', '75% - 100%')) +
-	theme_void() +
-	theme(legend.position = 'bottom') +
-	labs(fill = 'Proportion', title = 'Proportion of Democrats per voter registered as either a Democratic or Republican')
+MyLabels <-
+	USAMap %>%
+		group_by(state) %>%
+		summarize(mid_x = mean(x), mid_y = mean(y)) %>%
+		ungroup %>%
+		inner_join(StatewisePartyReg, by = 'state') %>%
+		mutate(label = scales::percent(adv_dem, 1))
+
+g <-
+	USAMap %>%
+		left_join(StatewisePartyReg, by = 'state') %>%
+		ggplot(aes(x = x, y = y)) +
+		geom_polygon(color = 'grey10', aes(fill = adv_dem, group = group)) +
+		geom_label_repel(data = MyLabels, aes(x = mid_x, y = mid_y, label = label)) +
+		scale_fill_gradient2(high = 'blue', mid = 'white', low = 'red', 
+			limits = c(-1, 1), labels = scales::percent_format(1), breaks = seq(-1, 1, by = 1)) +
+		theme_void() +
+		theme(legend.position = 'bottom') +
+		labs(title = 'Sanity check - respondents who are registered as either Democratic or Republican', fill = 'Democratic advantage')
+
+ggsave(filename = 'results/statewise_party_prop.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 # Compare to unified state govs
-
-USAMap %>% 
-	left_join(StateGovs, by = 'state') %>%
-	mutate(
-		party = if_else(is_state_gov_unified, party, '(Mixed)'),
-		party = ordered(party, levels = c('Democratic party', 'Republican party', '(Mixed)'))) %>%
-	ggplot(aes(x = x, y = y, fill = party, group = group)) +
-	geom_polygon() +
-	scale_fill_manual(values = c('blue', 'red', 'purple'),
-										breaks = c('Democratic party', 'Republican party', '(Mixed)')) +
-	theme_void() +
-	theme(legend.position = 'bottom') +
-	labs(title = 'States with unified local governments', fill = '')
+g <-
+	USAMap %>% 
+		left_join(StateGovs, by = 'state') %>%
+		mutate(
+			party = if_else(is_state_gov_unified, party, '(Not unified)'),
+			party = ordered(party, levels = c('Democratic party', 'Republican party', '(Not unified)'))) %>%
+		ggplot(aes(x = x, y = y, fill = party, group = group)) +
+		geom_polygon(color = 'grey10') +
+		scale_fill_manual(values = c('darkblue', 'darkred', 'darkpurple'),
+											breaks = c('Democratic party', 'Republican party', '(Mixed)')) +
+		theme_void() +
+		theme(legend.position = 'bottom') +
+		labs(title = 'States with unified local governments', fill = '')
+ggsave(filename = 'results/unified_state_govs.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 # sanity check- party registration v thermometers
 Data %>%
@@ -80,7 +102,10 @@ Data %>%
 	theme_light() +
 	theme(panel.grid.major.y = element_blank(),
 				legend.position = 'bottom') +
-	labs(x = 'Respondent\'s affiliation', y = 'Favorability', fill = '')
+	labs(x = 'Respondent\'s affiliation', y = 'Favorability', fill = 'Party in question')
+
+ggsave(filename = 'results/party_thermometers.png', plot = g, width = 16, height = 9, units = 'in')
+g
 #	
 # Is there a relationship between paying attention to politics and party of reg?
 ModelMe <-
@@ -100,10 +125,22 @@ ModelMe <-
 
 Items <- as.data.frame(ModelMe['How often do you pay attention to what’s going on in government and politics?'])
 gg <- ModelMe$party
-plot(likert(items = Items, grouping = gg))
+g <-
+	plot(likert(items = Items, grouping = gg)) + 
+		scale_y_continuous(labels=c("100%","50%","0%","50%","100%"),limits=c(-105,105), breaks = seq(-100, 100, by = 50))
+
+ggsave(filename = 'results/attention_by_party.png', plot = g, width = 16, height = 9, units = 'in')
+g
+
 #
 
 # How well do the thermometer questions hang together on different numbers of latent variables
+
+WeightAndAbstractItems %>%
+	mutate(weight )
+
+
+
 ## 1
 
 WeightAndAbstractItems <-
@@ -130,49 +167,82 @@ fa.parallel(Cor, n.obs = nrow(ModelMe), fa = 'fa')
 
 # fit FA with one component
 fa_fit1 <- fa(Cor, nfactors = 1, n.obs = nrow(Cor))
-fa_fit1$loadings
+fa_fit1
+
+g <-
+	fa_fit1$loadings %>%
+		unclass %>%
+		as.data.frame %>%
+		rownames_to_column(var = 'manifest_var') %>%
+		gather(latent_var, loading, -manifest_var) %>%
+		mutate_at('loading', round, 1) %>%
+		ggplot(aes(x = latent_var, y = manifest_var, label = loading)) +
+		geom_text() +
+		scale_y_discrete(limits = rev, labels = label_wrap_gen(width = 30)) +
+		theme_minimal() +
+		theme(axis.ticks = element_blank(),
+					panel.grid = element_blank(),
+					text = element_text(size = 7),
+					legend.position = 'none') +
+		labs(fill = '', 
+				 x = 'Latent variable', 
+				 y = 'Manifest variable')
+
+ggsave(filename = 'results/loadings_1.png', plot = g, width = 16/2, height = 9/2, units = 'in', bg = 'white')
+g
+#
 
 # fit FA with four components
 fa_fit4 <- fa(Cor, nfactors = 4, n.obs = nrow(Cor), rotate = 'promax')
+fa_fit4
 
-fa_fit4$loadings %>%
-	unclass %>%
-	as.data.frame %>%
-	rownames_to_column(var = 'manifest_var') %>%
-	gather(latent_var, loading, -manifest_var) %>%
-	mutate(loading = round(loading, 1)) %>%
-	group_by(manifest_var) %>%
-	mutate(is_primary_latent_var = abs(loading) == max(abs(loading))) %>%
-	ungroup %>%
-	ggplot(aes(x = latent_var, y = manifest_var, fill = is_primary_latent_var, label = loading)) +
-	geom_tile() +
-	geom_text() +
-	scale_fill_manual(values = c('white', 'lightblue')) +
-	scale_y_discrete(limits = rev, labels = label_wrap_gen(width = 30)) +
-	theme_minimal() +
-	theme(axis.ticks = element_blank(),
-				panel.grid = element_blank(),
-				text = element_text(size = 7),
-				legend.position = 'none') +
-	labs(fill = '', 
-			 x = 'Latent variable', 
-			 y = 'Manifest variable')
-#
+g <-
+	fa_fit4$loadings %>%
+		unclass %>%
+		as.data.frame %>%
+		rownames_to_column(var = 'manifest_var') %>%
+		gather(latent_var, loading, -manifest_var) %>%
+		mutate(loading = round(loading, 1)) %>%
+		group_by(manifest_var) %>%
+		mutate(is_primary_latent_var = abs(loading) == max(abs(loading))) %>%
+		ungroup %>%
+		ggplot(aes(x = latent_var, y = manifest_var, fill = is_primary_latent_var, label = loading)) +
+		geom_tile() +
+		geom_text() +
+		scale_fill_manual(values = c('white', 'lightblue')) +
+		scale_y_discrete(limits = rev, labels = label_wrap_gen(width = 30)) +
+		theme_minimal() +
+		theme(axis.ticks = element_blank(),
+					panel.grid = element_blank(),
+					text = element_text(size = 7),
+					legend.position = 'none') +
+		labs(fill = '', 
+				 x = 'Latent variable', 
+				 y = 'Manifest variable')
+
+ggsave(filename = 'results/loadings_4.png', plot = g, width = 16, height = 9, units = 'in')
+g
+	#
 anova(fa_fit1, fa_fit4)  # best to stick with 1 factor
-
 
 # Look at the culture issues, overall and broken out by the four party affiliations ####
 
 ## Abortion
-Items <-
-	Data %>%
-		select(weight, Abortion = 'PRE: STD ABORTION: SELF-PLACEMENT') %>%
-		drop_na %>%
-		mutate(weight = round(weight * 100)) %>%
-		uncount(weight) %>%
-		as.data.frame
-plot(likert(items = Items)) +
-	guides(fill = guide_legend(nrow = 2))
+g <-
+	Items <-
+		Data %>%
+			select(weight, Abortion = 'PRE: STD ABORTION: SELF-PLACEMENT') %>%
+			drop_na %>%
+			mutate(weight = round(weight * 100)) %>%
+			uncount(weight) %>%
+			as.data.frame
+	plot(likert(items = Items)) +
+		guides(fill = guide_legend(nrow = 2)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105),
+											 labels = as_mapper(~str_c(.x, '%')))
+ggsave(filename = 'results/abortion.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 ModelMe <-
 	Data %>%
@@ -180,12 +250,20 @@ ModelMe <-
 		drop_na %>%
 		mutate(weight = round(weight * 100)) %>%
 		uncount(weight)
+
 Items <- as.data.frame(ModelMe['Abortion'])
 gg <- ModelMe$party
-plot(likert(items = Items, grouping = gg)) +
-	scale_x_discrete(limits = rev) +
-	guides(fill = guide_legend(nrow = 2))
 
+g <-
+	plot(likert(items = Items, grouping = gg)) +
+		scale_x_discrete(limits = rev) +
+		guides(fill = guide_legend(nrow = 2)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105),
+											 labels = as_mapper(~str_c(.x, '%')))
+ggsave(filename = 'results/abortion_by_party.png', plot = g, width = 16, height = 9, units = 'in')
+g
+	
 ## Services to same-sex couples
 Items <-
 	Data %>%
@@ -195,8 +273,15 @@ Items <-
 			weight = round(weight * 100)) %>%
 		uncount(weight) %>%
 		as.data.frame
-plot(likert(items = Items)) +
-	guides(fill = guide_legend(nrow = 1))
+
+g <-
+	plot(likert(items = Items)) +
+		guides(fill = guide_legend(nrow = 1)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105),
+											 labels = as_mapper(~str_c(.x, '%')))
+ggsave(filename = 'results/services_to_same_sex_couples.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 ModelMe <-
 	Data %>%
@@ -207,9 +292,15 @@ ModelMe <-
 		uncount(weight)
 Items <- as.data.frame(ModelMe['Services to same-sex couples'])
 gg <- ModelMe$party
-plot(likert(items = Items, grouping = gg)) +
-	scale_x_discrete(limits = rev) +
-	guides(fill = guide_legend(nrow = 1))
+g <-
+	plot(likert(items = Items, grouping = gg)) +
+		scale_x_discrete(limits = rev) +
+		guides(fill = guide_legend(nrow = 1)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105),
+											 labels = as_mapper(~str_c(.x, '%')))
+ggsave(filename = 'results/services_to_same_sex_couples_by_party.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 ## Transgender policy
 Items <-
@@ -219,8 +310,15 @@ Items <-
 		mutate(weight = round(weight * 100)) %>%
 		uncount(weight) %>%
 		as.data.frame
-plot(likert(items = Items)) +
-	guides(fill = guide_legend(nrow = 1))
+
+g <-
+	plot(likert(items = Items)) +
+		guides(fill = guide_legend(nrow = 1)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105), 
+											 labels = as_mapper(~str_c(.x, '%'))) 
+ggsave(filename = 'results/transgender_bathroom.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 ModelMe <-
 	Data %>%
@@ -231,9 +329,15 @@ ModelMe <-
 		uncount(weight)
 Items <- as.data.frame(ModelMe['Transgender policy'])
 gg <- ModelMe$party
-plot(likert(items = Items, grouping = gg)) +
-	scale_x_discrete(limits = rev) +
-	guides(fill = guide_legend(nrow = 1))
+g <-
+	plot(likert(items = Items, grouping = gg)) +
+		scale_x_discrete(limits = rev) +
+		guides(fill = guide_legend(nrow = 1)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105), 
+											 labels = as_mapper(~str_c(.x, '%'))) 
+ggsave(filename = 'results/transgender_bathroom_by_party.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 # Laws to protect gays and lesbians against job descrimination
 Items <-
@@ -244,8 +348,14 @@ Items <-
 			weight = round(weight * 100)) %>%
 		uncount(weight) %>%
 		as.data.frame
-plot(likert(items = Items)) +
-	guides(fill = guide_legend(nrow = 1))
+g <-
+	plot(likert(items = Items)) +
+		guides(fill = guide_legend(nrow = 1))
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105), 
+											 labels = as_mapper(~str_c(.x, '%'))) 
+ggsave(filename = 'results/gay_job.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 ModelMe <-
 	Data %>%
@@ -256,9 +366,15 @@ ModelMe <-
 		uncount(weight)
 Items <- as.data.frame(ModelMe['Laws to protect gays and lesbians against job discrimination'])
 gg <- ModelMe$party
-plot(likert(items = Items, grouping = gg)) +
-	scale_x_discrete(limits = rev) +
-	guides(fill = guide_legend(nrow = 1))
+g <-
+	plot(likert(items = Items, grouping = gg)) +
+		scale_x_discrete(limits = rev) +
+		guides(fill = guide_legend(nrow = 1)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105), 
+											 labels = as_mapper(~str_c(.x, '%'))) 
+ggsave(filename = 'results/gay_job_by_party.png', plot = g, width = 16, height = 9, units = 'in')
+g
 
 # Unauthorized immigrants
 Items <-
@@ -269,8 +385,15 @@ Items <-
 			weight = round(weight * 100)) %>%
 		uncount(weight) %>%
 		as.data.frame
-plot(likert(items = Items)) +
-	guides(fill = guide_legend(nrow = 1))
+g <-
+	plot(likert(items = Items)) +
+		guides(fill = guide_legend(nrow = 1)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105), 
+											 labels = as_mapper(~str_c(.x, '%'))) 
+ggsave(filename = 'results/immigrants.png', plot = g, width = 16, height = 9, units = 'in')
+g
+
 
 ModelMe <-
 	Data %>%
@@ -281,9 +404,15 @@ ModelMe <-
 		uncount(weight)
 Items <- as.data.frame(ModelMe['Policy toward unauthorized immigrants'])
 gg <- ModelMe$party
-plot(likert(items = Items, grouping = gg)) +
-	scale_x_discrete(limits = rev) +
-	guides(fill = guide_legend(nrow = 1))
+g <-
+	plot(likert(items = Items, grouping = gg)) +
+		scale_x_discrete(limits = rev) +
+		guides(fill = guide_legend(nrow = 2)) +
+		scale_y_continuous(breaks = seq(-100, 100, by = 50),
+											 limits = c(-105, 105), 
+											 labels = as_mapper(~str_c(.x, '%'))) 
+ggsave(filename = 'results/immigrants_by_party.png', plot = g, width = 16, height = 9, units = 'in')
+g
 #
 
 # Compare abortion feelings per state
@@ -294,22 +423,17 @@ Data %>%
 		state,
 		abortion = contains('ABORTION')) %>%
 	drop_na %>%
-	mutate(
-		no_abortion = abortion == 'By law, abortion should never be permitted',
-		weight = round(weight * 100)) %>%
-	uncount(weight) %>%
+	mutate(no_abortion = abortion == 'By law, abortion should never be permitted') %>%
 	group_by(state) %>%
-	summarize(prop_no_abortion = mean(no_abortion)) %>%
+	summarize(weighted_mean = weighted.mean(x = no_abortion, w = weight)) %>%
 	ungroup %>%
-	mutate(binned_no_abortion = cut_width(prop_no_abortion, width = 0.1, boundary = 0, ordered_levels = T)) %>%
+	mutate(bin = cut_width(weighted_mean, width = 0.1, boundary = 0, ordered_result = T)) %>%
 	right_join(USAMap, by = 'state') %>%
-	ggplot(aes(x = x, y = y, group = group, fill = binned_no_abortion)) +
-	geom_polygon(color = 'black') +
-	scale_fill_viridis_d(
-										breaks = c('[0,0.1]', '(0.1,0.2]', '(0.2,0.3]', '(0.4,0.5]'),
-										labels = c('0% - 10%', '10% - 20%', '20% - 30%', '40% - 50%'), 
-										na.value = NA) +
+	ggplot(aes(x = x, y = y, group = group, fill = bin)) +
+	geom_polygon(color = 'darkgrey') +
+	scale_fill_brewer(palette = 'OrRd', direction = 1, labels = c('0% - 10%', '10% - 20%', '20% - 30%', '30% - 40%')) +
 	theme_void() +
+	theme(legend.position = 'bottom') +
 	labs(fill = '', title = 'Respondents who indicated that by law, abortion should never be permitted')
 
 # Which states have the biggest disagreement between republican voters and the rest wrt abortion	
